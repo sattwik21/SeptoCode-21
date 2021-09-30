f056d8e5f4f614f021126a1ed571b50a019fd6b8
Sattwik Palai <sattwikpalai21@gmail.com>
2021-09-30 16:51:07 +0530

    Change variable name

--- Solution/SattwikPalai_day9.py
+++ Solution/SattwikPalai_day9.py
@@ -2,16 +2,16 @@ def line():
     t = int(input())
     while t>0:
         s = input()
-        flag = 0
+        fla = 0
         x = s[0]
 
         for i in s:
             if i==x:
-                flag = flag + 1
+                fla = fla + 1
             else:
                 x = i
 
-        print(flag-1)
+        print(fla-1)
 
         t -= 1
 
